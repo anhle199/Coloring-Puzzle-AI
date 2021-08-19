f59ee2bdccde968ca5e6a15d25a2a929a71271bb
NguyenHuaHung <63572298+huahungnguyen121@users.noreply.github.com>
2021-08-19 20:51:37 +0700

    Improve UI

--- source/UI.py
+++ source/UI.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from tkinter.constants import DISABLED, LEFT, NORMAL, RIGHT, X, END
+from tkinter.constants import BOTH, BOTTOM, DISABLED, LEFT, NORMAL, RIGHT, X, END, Y
 from tkinter.filedialog import askopenfilename
 from utilities.file_io import *
-from utilities.constants import CellStatus, CellSize, Algorithm
+from utilities.constants import CellStatus, CellSize, Algorithm, ScrollConst
 import pysat_solution
 import backtracking
 
@@ -10,25 +10,31 @@ import backtracking
 def GUI():
     root = tk.Tk()
     root.title('Coloring Puzzle - AI HCMUS')
+
+    # General Frame
     top = tk.Frame(root)
     top.pack(pady=10)
     mid = tk.Frame(root)
     mid.pack(fill=X, padx=10, pady=5)
-    bot = tk.Frame(root)
-    bot.pack()
+    bot = tk.Frame(root, borderwidth=1, relief='solid')
+    bot.pack(fill=BOTH, expand=True, padx=10, pady=10)
+    foot = tk.Frame(bot)
+    foot.pack(fill=X, side=BOTTOM)
+    right = tk.Frame(bot)
+    right.pack(fill=Y, side=RIGHT)
 
-    # variables
-    algoMode = ['PySat', 'A Star', 'Brute Force', 'Backtracking', 'None']
+    # variables for using
+    algoMode = ['PySat', 'A Star', 'BruteForce', 'Backtracking', 'None']
     curMode = -1
     matrix = []
 
-    def handleGetFile():
+    def handleGetFile(): # Get file's path
         path = askopenfilename()
         filePath.delete(0, END)
         filePath.insert(0, path)
         return
 
-    def handleCredit():
+    def handleCredit(): # Show credit information
         popup = tk.Tk()
         popup.title('Credit')
         creditText = tk.Label(popup, text='Project 2: Coloring Puzzle', font=('Arial', 15))
@@ -42,7 +48,7 @@ def GUI():
         popup.geometry('300x200+%d+%d' % (root.winfo_screenwidth() / 2 - 150, root.winfo_screenheight() / 2 - 100))
         return
 
-    def handleDisplayArray():
+    def handleDisplayArray(): # Load puzzle array
         nonlocal matrix
         path = filePath.get()
         if (len(path) == 0):
@@ -59,41 +65,26 @@ def GUI():
             size = (len(matrix), len(matrix[0]))
         except FileNotFoundError:
             warning.config(text='File does not exist!!!!', fg='red')
+            okButton['state'] = NORMAL
             return
         except ValueError:
-            warning.config(text='File does not exist!!!!', fg='red')
+            warning.config(text='Error in input file format!!!!', fg='red')
+            okButton['state'] = NORMAL
             return
 
-        width = root.winfo_width()
-        height = root.winfo_height()
-        hSum = 0
-        wSum = 0
         for i in range(size[0]):
-            h = 0
-            w = 0
             for j in range(size[1]):
                 cell = ' '
                 if matrix[i][j] != -1:
                     cell = str(matrix[i][j])
-                box = tk.Label(array, text=cell, width=CellSize.WIDTH, height=CellSize.HEIGHT, borderwidth=2, relief='solid', font=('Arial', 20))
+                box = tk.Label(array, text=cell, width=CellSize.WIDTH, height=CellSize.HEIGHT, borderwidth=2, relief='solid', font=('Arial', CellSize.FONTSIZE))
                 box.grid(row=i, column=j)
-                box.update()
-                h = box.winfo_height()
-                w += box.winfo_width()
-            hSum += h
-            wSum = w
-        if wSum > width:
-            width += wSum - width + 20
-        if hSum > height - 100:
-            height += hSum - height + root.winfo_height() + 20
-
-        root.geometry('%dx%d+%d+%d' % (width, height, root.winfo_screenwidth() / 2 - width / 2, root.winfo_screenheight() / 2 - (height + 10) / 2))
 
         warning.config(text='Load successfully', fg='green')
         okButton['state'] = NORMAL
         return
 
-    def handleSelectAlgo():
+    def handleSelectAlgo(): # Select algorithm for running
         popup = tk.Tk()
         nonlocal curMode
         mode = tk.StringVar(popup, value=str(curMode))
@@ -106,12 +97,13 @@ def GUI():
             ('None', '-1'),
             ('PySat', '0'),
             ('A Star', '1'),
-            ('Brute Force', '2'),
+            ('BruteForce', '2'),
             ('Backtracking', '3')
-            # ('PySat', str(Algorithm.PYSAT)),
-            # ('A Star', str(Algorithm.A_STAR)),
-            # ('Brute Force', str(Algorithm.BRUTE_FORCE)),
-            # ('Backtracking', str(Algorithm.BACKTRACKING))
+            # ('None', Algorithm.PYSAT),
+            # ('PySat', Algorithm.PYSAT),
+            # ('A Star', Algorithm.A_STAR),
+            # ('BruteForce', Algorithm.BRUTE_FORCE),
+            # ('Backtracking', Algorithm.BACKTRACKING)
         )
         def onClick():
             return
@@ -120,11 +112,8 @@ def GUI():
             curMode = int(mode.get())
             algoTitle.config(text='Selected Algorithm: {}'.format(algoMode[curMode]))
             popup.destroy()
-        i = 0
-        for item in values:
+        for i, item in zip(range(len(values)), values):
             tk.Radiobutton(optionWrapper, text=item[0], variable=mode, value=item[1], command=onClick).grid(row=0, column=i)
-            i += 1
-        # confirmButton = tk.Button(popup, text='Confirm', command=handleConfirm, background='green', fg='white')
         confirmButton = tk.Button(popup, text='Confirm', command=handleConfirm)
         confirmButton.pack(side=RIGHT, padx=10)
         popup.geometry('450x150+%d+%d' % (root.winfo_screenwidth() / 2 - 225, root.winfo_screenheight() / 2 - 75))
@@ -134,25 +123,31 @@ def GUI():
         if (curMode == -1):
             warning.config(text='Please select an algorithm!!!', fg='red')
         else:
-            warning.config(text='Running {} ...'.format(algoMode[curMode]), fg='blue')
+            if (len(matrix) == 0):
+                warning.config(text='Please load the puzzle first!!!', fg='red')
+                return
+            warning.config(text='Running {} .....'.format(algoMode[curMode]), fg='blue')
 
             model = None
+            run = False
             if curMode == Algorithm.PYSAT:
                 model = pysat_solution.solve(matrix)
+                run = True
             elif curMode == Algorithm.A_STAR:
-                warning.config(text='Not implemented {} yet'.format(algoMode[curMode]), fg='red')
+                warning.config(text='{} has not been implemented yet'.format(algoMode[curMode]), fg='red')
             elif curMode == Algorithm.BRUTE_FORCE:
-                warning.config(text='Not implemented {} yet'.format(algoMode[curMode]), fg='red')
+                warning.config(text='{} has not been implemented yet'.format(algoMode[curMode]), fg='red')
             elif curMode == Algorithm.BACKTRACKING:
                 model = backtracking.solve(matrix)
-
-            if model == None:
-                warning.config(text='No solution with {}'.format(algoMode[curMode]), fg='green')
-            else:
-                for widget, num in zip(array.winfo_children(), model):
-                    color = 'green' if num > 0 else 'red'
-                    widget.config(bg=color, fg='white')
-                warning.config(text='Run {} successfully'.format(algoMode[curMode]), fg='green')
+                run = True
+            if run == True:
+                if model == None:
+                    warning.config(text='No solution with {}'.format(algoMode[curMode]), fg='green')
+                else:
+                    for widget, num in zip(array.winfo_children(), model):
+                        color = 'green' if num > 0 else 'red'
+                        widget.config(bg=color, fg='white')
+                    warning.config(text='Run {} successfully'.format(algoMode[curMode]), fg='green')
 
         return
 
@@ -167,21 +162,41 @@ def GUI():
     okButton = tk.Button(topRight, text='Load Puzzle', fg='black', command=handleDisplayArray, width=13)
     okButton.pack(pady=5)
 
-    # runButton = tk.Button(topRight, text='Run', bg='green', fg='white', command=handleRunAlgo, width=13)
     runButton = tk.Button(topRight, text='Run', command=handleRunAlgo, width=13)
     runButton.pack(pady=5)
 
     credit = tk.Button(topRight, text='Credit', fg='black', command=handleCredit, width=13)
     credit.pack(pady=5)
 
+    # Notification while running
     status = tk.Label(mid, text='Notification:')
     status.pack(side=LEFT)
     warning = tk.Label(mid, text='None', fg='black')
     warning.pack(side=LEFT)
 
-    array = tk.Frame(bot)
-    array.pack()
+    # Area for display data
+    canvas = tk.Canvas(bot)
+    canvas.pack(fill=BOTH, expand=True, side=LEFT)
+    scrollY = tk.Scrollbar(right, command=canvas.yview)
+    scrollY.pack(fill=Y, expand=True)
+    scrollX = tk.Scrollbar(foot, orient="horizontal", command=canvas.xview)
+    scrollX.pack(fill=X, expand=True)
+    array = tk.Frame(canvas)
 
+    # Bind event for scroll
+    canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    array.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    def scrollWithMouse(event):
+        canvas.yview_scroll(int(-1 * (event.delta / ScrollConst.MODIFIER)) , "units")
+    canvas.bind_all("<MouseWheel>", scrollWithMouse)
+
+    # Config scroll
+    canvas.configure(yscrollcommand=scrollY.set, xscrollcommand=scrollX.set)
+
+    #Create window frame
+    canvas.create_window((0, 0), window=array, anchor="nw")
+
+    # Command Infomation (TOP LEFT)
     pathTitle = tk.Label(topLeft, text='Path', justify=LEFT)
     filePath = tk.Entry(topLeft, width=50)
     pathTitle.pack()
@@ -191,8 +206,9 @@ def GUI():
     algoButton = tk.Button(topLeft, text='Select Algorithm', fg='black', command=handleSelectAlgo, width=13)
     algoButton.pack(side=RIGHT, pady=10)
 
-    top.update()
-    mid.update()
-    root.geometry('%dx%d+%d+%d' % (top.winfo_width(), top.winfo_height() + mid.winfo_height() + 30, root.winfo_screenwidth() / 2 - top.winfo_width() / 2, root.winfo_screenheight() / 2 - (top.winfo_height() + mid.winfo_height() + 30) / 2))
+    # main window size
+    width = 1000 if root.winfo_screenwidth() > 1000 else root.winfo_screenwidth()
+    height = 900 if root.winfo_screenheight() > 900 else root.winfo_screenheight()
+    root.geometry('%dx%d+%d+%d' % (width, height, root.winfo_screenwidth() / 2 - width / 2, root.winfo_screenheight() / 2 - height / 2))
     root.update()
     root.mainloop()
--- source/utilities/constants.py
+++ source/utilities/constants.py
@@ -8,12 +8,20 @@ class CellStatus:
 class CellSize:
     WIDTH = 9
     HEIGHT = 4
+    FONTSIZE = 10
     if platform.system() == 'Darwin':
         WIDTH = 5
         HEIGHT = 2
+        FONTSIZE = 20
 
 class Algorithm:
+    NONE = -1
     PYSAT = 0
     A_STAR = 1
     BRUTE_FORCE = 2
     BACKTRACKING = 3
+
+class ScrollConst:
+    MODIFIER = 120
+    if platform.system() == 'Darwin':
+        MODIFIER = 1
